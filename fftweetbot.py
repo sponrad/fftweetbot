5aba27e3b17fcecc77e4cfa99f8abb9dcd6f96ec
Conrad <sponrad@gmail.com>
2016-03-08 11:13:45 -0800

    fetching some tweets

--- fftweetbot.py
+++ fftweetbot.py
@@ -12,19 +12,12 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
  
-twts = api.search(q="Hello World!")     
- 
-#list of specific strings we want to check for in Tweets
-t = ['Hello world!',
-    'Hello World!',
-    'Hello World!!!',
-    'Hello world!!!',
-    'Hello, world!',
-    'Hello, World!']
- 
-for s in twt:
-    for i in t:
-        if i == s.text:
-            sn = s.user.screen_name
-            m = "@%s Hello!" % (sn)
-            s = api.update_status(m, s.id)
+query = "clippersnation"
+
+for tweet in tweepy.Cursor(api.search,
+                           q=query,
+                           rpp=5,
+                           result_type="recent",
+                           include_entities=True,
+                           lang="en").items():
+    print tweet.created_at, tweet.text
