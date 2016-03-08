70df1eea8c7debddcfa7582fb083509e2178ecff
Conrad <sponrad@gmail.com>
2016-03-08 15:23:58 -0800

    unsure of twitter

--- fftweetbot.py
+++ fftweetbot.py
@@ -12,12 +12,27 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
  
-query = "clippersnation"
+query = "bullsnation"
+max_tweets = 1
 
 for tweet in tweepy.Cursor(api.search,
                            q=query,
                            rpp=5,
                            result_type="recent",
                            include_entities=True,
-                           lang="en").items():
-    print tweet.created_at, tweet.text
+                           lang="en").items(max_tweets):
+    #print tweet.id, tweet.created_at, tweet.text
+    #print tweet.author, tweet.user
+    
+    #api.create_favorite(tweet.id)
+
+    #api.send_direct_message(user_id=tweet.user.id, text="YO DOG check me out!")
+
+    """ Need to figure out what I want this bot to accomplish """
+    """ given a list of search terms do something """
+
+
+#api.send_direct_message(user="username", text="YO DOG check me out!")
+
+
+
